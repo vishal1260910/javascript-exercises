dbf943fc8a2ed64a2a96b28cfc8032a23b82b144
Vishal <kvishal126@gmail.com>
2024-10-11 18:05:59 +0530

    Solved first three exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const arr = Array.from(string);
+    //console.log(arr);
+    let revarr = [];
+    for(let i =arr.length-1; i>=0; i--){
+        revarr.push(arr[i]);
+    }
+    return revarr.join("");
+    
+        
+        
+    }
+    
 
-};
+
+reverseString("vishal");
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
