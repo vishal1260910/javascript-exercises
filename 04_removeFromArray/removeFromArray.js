b302cd555a31944f546d2c7c1824dadfb2e6411f
Vishal <kvishal126@gmail.com>
2024-10-13 00:01:47 +0530

    Implemented the remove array function Implemented using array methods includes toSpliced It is prefferable to use backward loop when iterating and removing elements from arrays.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    let newArr = array;
+    for(let i = array.length-1; i>=0; i--){
+        if(items.includes(array[i]))
+            newArr = newArr.toSpliced(i,1);
+    }
+            
+                
+    
+   return newArr;
 };
+
+removeFromArray([1,2,3],3,2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
