9a5b300ea8c4b916e36b09a4e7b292b598b84252
Vishal <kvishal126@gmail.com>
2024-10-13 17:21:23 +0530

    Implemented the leapYear fumction Using the two conditions that a year must satisfy in order to be considered a leap year.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+	if ((year %4===0)&&(year%100!=0)||(year%400===0)){
+		return true;
+	}
+
+		return false;
+
 
 };
+console.log(leapYears(1900));
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// A year is a leap year if:
+
+// It is divisible by 4 and:
+// It is not divisible by 100, or
+// It is divisible by 400.
