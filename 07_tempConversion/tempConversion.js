40e3a1529cb49b9af387447d7fab93f8104e4738
Vishal <kvishal126@gmail.com>
2024-10-13 17:22:36 +0530

    Implemented temp conversion using 1 .toFixed() method 2 .parseFloat() function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,33 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let tempInCelsius = (5/9*(F-32));
+  finTempinCelsius=tempInCelsius.toFixed(1);
+  return parseFloat(finTempinCelsius)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let tempinFahrenheit=(9/5*C)+32;
+  let finTempinF=tempinFahrenheit.toFixed(1)
+  return parseFloat(finTempinF);
+
 };
+
+console.log(convertToCelsius(-100));
+console.log(convertToFahrenheit(100));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+
+
+
+
+
+
+
+
+//F=(9/5*c)+32
+//C=(5/9*(F-32))
